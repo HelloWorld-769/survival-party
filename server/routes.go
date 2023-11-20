9db0ceee4299d784c547394923c510270642b4c3
AmanTarar <aman.tarar@chicmic.co.in>
2023-11-20 15:45:28 +0530

    feat/ ping route added for server testing

--- server/routes.go
+++ server/routes.go
@@ -2,10 +2,9 @@ package server
 
 import (
 	_ "main/docs"
-	"main/server/gateway"
-	"main/server/handler"
 	gomail "main/server/services/alert_service/Gomail"
 
+	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
@@ -23,4 +22,15 @@ func ConfigureRoutes(server *Server) {
 	server.engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	server.engine.POST("/send-email", gomail.SendEmailOtpService)
+	server.engine.POST("/send-sms", handler.TwilioServiceHnadler)
+
+	server.engine.GET("/ping", Pong)
+
+}
+
+// For server Testing(acknowledgement)
+func Pong(ctx *gin.Context) {
+
+	msg := "Pong"
+	ctx.Writer.Write([]byte(msg))
 }
