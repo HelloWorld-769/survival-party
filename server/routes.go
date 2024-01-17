d0689fc05e23ba5210342e9a76340177d7d9873b
AmanTarar <aman.tarar@chicmic.co.n>
2024-01-17 13:23:25 +0530

    fix/minor change in route

--- server/routes.go
+++ server/routes.go
@@ -4,6 +4,7 @@ import (
 	_ "main/docs"
 	"main/server/gateway"
 	"main/server/handler"
+	"main/server/services/rewards"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -49,4 +50,5 @@ func ConfigureRoutes(server *Server) {
 	//daily rewards
 	server.engine.PUT("/api/v1/collect-daily-rewards", gateway.UserAuthorization, handler.CollectDailyRewardHandler)
 	server.engine.GET("/api/v1/daily-rewards", gateway.UserAuthorization, handler.GetUserDailyRewardDataHandler)
+	server.engine.GET("/api/v1/timeLeft", rewards.DailyRewardTimeLeft)
 }
